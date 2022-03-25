dc4486ef4cf136b84a3d3faa9982f3649dac6fdb
JONGGIL LEE <brianlee2k@naver.com>
2022-03-25 18:43:33 +0900

    버킷리스트 삭제하기

--- src/Detail.js
+++ src/Detail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useHistory, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteBucket } from "./redux/modules/bucket";
 
 const Detail = (props) => {
   const history = useHistory();
@@ -8,21 +9,25 @@ const Detail = (props) => {
   const params = useParams();
   const bucket_index = params.index_num;
   const bucket_list = useSelector((state) => state.bucket.list);
+  const dispatch = useDispatch();
 
   console.log(bucket_list);
   console.log(bucket_index);
   console.log(bucket_list[bucket_index]);
   return (
     <>
-      <div align="center">{bucket_list[bucket_index]}</div>
-
-      <button
-        onClick={() => {
-          history.goBack();
-        }}
-      >
-        뒤로가기
-      </button>
+      <div>
+        <h1>{bucket_list[bucket_index]}</h1>
+        <button
+          onClick={() => {
+            console.log("삭제하기 버튼 누름");
+            dispatch(deleteBucket(bucket_index));
+            history.goBack();
+          }}
+        >
+          삭제!
+        </button>
+      </div>
     </>
   );
 };
--- src/redux/modules/bucket.js
+++ src/redux/modules/bucket.js
@@ -2,6 +2,7 @@
 
 // Actions
 const CREATE = "bucket/CREATE";
+const DELETE = "bucket/DELETE";
 
 const initialState = {
   list: ["영화관 가기", "매일 책읽기", "수영 배우기", "고양이랑 놀기"],
@@ -11,6 +12,11 @@ const initialState = {
 export function createBucket(bucket) {
   console.log("액션을 생성할거야");
   return { type: CREATE, bucket: bucket };
+}
+
+export function deleteBucket(bucket_index) {
+  console.log("지울 버킷 인덱스?", bucket_index);
+  return { type: DELETE, bucket_index };
 }
 
 // export function createWidget(widget) {
@@ -25,6 +31,20 @@ export default function reducer(state = initialState, action = {}) {
     case "bucket/CREATE": {
       console.log("이제 값을 바꿀거야");
       const new_bucket_list = [...state.list, action.bucket];
+      return { list: new_bucket_list };
+    }
+
+    case "bucket/DELETE": {
+      console.log(action, state);
+      const new_bucket_list = state.list.filter((l, idx) => {
+        console.log(
+          parseInt(action.bucket_index) !== idx,
+          action.bucket_index,
+          idx
+        );
+        return parseInt(action.bucket_index) !== idx;
+      });
+      console.log(new_bucket_list);
       return { list: new_bucket_list };
     }
     default:
